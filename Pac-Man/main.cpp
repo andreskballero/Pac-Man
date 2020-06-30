c866471df13d1be894525dae1dd9dc60260b1c67
andreskballero <acaballerotoledo@gmail.com>
2020-06-30 14:06:57 +0200

    Initial Commit

---
+++ Pac-Man/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Pac-Man
+//
+//  Created by Andrés Caballero Toledo on 30/06/2020.
+//  Copyright © 2020 act. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
